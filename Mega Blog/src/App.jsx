e3e1f28e7a155d9a47569be5ae1f3a64d9af72e2
Abdul-Rehman <100258023+Abdul-Rehman111@users.noreply.github.com>
2025-04-12 12:55:16 +0500

    Add files via upload

---
+++ Mega Blog/src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+
+  //when the application is loaded we use useEffect to configure while the user is login or not
+  
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+
+        //.finally is the method which always run whether the above method is loaded or not
+
+    .finally(() => setLoading(false))
+  }, [])
+
+  
+  //Modern if else is used below
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
+}
+
+export default App
